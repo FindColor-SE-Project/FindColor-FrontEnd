7140082870dbee7d64459985775993d56450cdec
BT <kornkanok_kanchana@cmu.ac.th>
2024-06-04 15:49:10 +0700

    Fix GetSeasons page

--- frontend/src/view/GetSeasons.vue
+++ frontend/src/view/GetSeasons.vue
@@ -6,10 +6,10 @@ import { ref } from 'vue'
     <div class="upload__container">
             <div class="upload__area">
                 <div class="icon__upload"><font-awesome-icon icon="cloud-arrow-up" /></div>
-                <button>Choose Image to Upload</button>
+                <button class="josefin-sans-font">Choose Image to Upload</button>
                 <input type="file" hidden>
-                <button>Take a photo to Upload</button>
-                <p>or Drag and Drop image to Upload</p>
+                <button class="josefin-sans-font">Take a photo to Upload</button>
+                <p class="josefin-sans-font">or Drag and Drop image to Upload</p>
             </div>
     </div>
 
@@ -28,23 +28,27 @@ import { ref } from 'vue'
     width: 850px;
     height: 500px;
     padding: 30px;
-    background: powderblue;
     text-align: center;
     border-radius: 20px;
     flex-direction: column;
     display: flex;
     align-items: center;
     justify-content: center;
-
+    border: #EDC2D8 dashed 5px;
 }
 
 .icon__upload {
-    font-size: 65px;
+    font-size: 70px;
+    color: #8ABAD3;
 }
 
 button {
     font-size: 28px;
     margin: 5px 0;
+    padding: 6px 10px;
+    border: black solid 1px;
+    border-radius: 20px;
+    background-color: #EDC2D8;
 }
 
 p {
@@ -52,4 +56,7 @@ p {
     margin: 5px 0;
 }
 
+button:hover {
+    background-color: #FCF6F5;
+}
 </style>

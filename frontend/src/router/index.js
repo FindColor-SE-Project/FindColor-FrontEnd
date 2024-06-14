48de2226689933ece92a7eedf6e3320f94714f7f
BT <kornkanok_kanchana@cmu.ac.th>
2024-06-14 17:09:43 +0700

    Add Layout for Category in each BrandDetailView

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from '@/views/Home.vue'
 import BrandListView from '@/views/BrandListView.vue'
 import GetSeasonsPage from '@/views/GetSeasons.vue'
-import ProductListView from '@/views/ProductListView.vue'
+import ProductListView from '@/views/Category/ProductListView.vue'
+import LipsView from '@/views/Category/LipsView.vue'
+import EyeshadowView from '@/views/Category/EyeshadowView.vue'
+import BlushView from '@/views/Category/BlushView.vue'
+import ProductLayout from '@/views/Category/ProductLayout.vue'
 
 const routes = [
     {
@@ -22,8 +26,33 @@ const routes = [
     },
     {
         path: '/brandListView/:brandName',
-        name: 'productListView',
-        component: ProductListView,
+        name: 'productLayout',
+        component: ProductLayout,
+        props: true,
+        children: [
+            {
+                path: '',
+                name: 'productListView',
+                component: ProductListView,
+                props: true
+            },
+            {
+                path: 'Lips',
+                name: 'lipsView',
+                component: LipsView,
+                props: true
+            },            {
+                path: 'Blush',
+                name: 'blushView',
+                component: BlushView,
+                props: true
+            },            {
+                path: 'Eyeshadow',
+                name: 'eyeshadowView',
+                component: EyeshadowView,
+                props: true
+            }, 
+        ]
     }
 ]
 
---
+++ frontend/src/views/Category/ProductLayout.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <h1 class="card-title cardo-regular">{{ brandName }}</h1>
+      <div class="category-select">
+        <router-link class="josefin-sans-font category-layout" :to="{ name: 'productListView' }">All</router-link>
+        <router-link class="josefin-sans-font category-layout" :to="{ name: 'lipsView' }">Lips</router-link>
+        <router-link class="josefin-sans-font category-layout" :to="{ name: 'blushView' }">Blush</router-link>
+        <router-link class="josefin-sans-font category-layout" :to="{ name: 'eyeshadowView' }">Eyeshadow</router-link>
+      </div>
+      <!-- <div class="product-list">
+        <ProductCard v-for="product in filteredProducts" :key="product.productID" :product="product" />
+      </div> -->
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { ref, computed, onMounted, watch } from 'vue';
+import ProductCard from '@/components/ProductCard.vue';
+import axios from 'axios';
+import { useRoute } from 'vue-router'
+
+export default {
+  components: {
+    ProductCard
+  },
+  setup () {
+    const route = useRoute();
+    const products = ref([]);
+    const brandName = computed(() => route.params.brandName);
+
+    const filteredProducts = computed(() => {
+      return products.value.filter(product => product.brandName === brandName.value);
+    });
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/data');
+        products.value = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    onMounted(fetchData);
+
+    watch(
+      () => route.params.brandName,
+      async (newBrandName, oldBrandName) => {
+        if (newBrandName !== oldBrandName) {
+          await fetchData();
+        }
+      }
+    );
+
+    return {
+      products,
+      brandName,
+      filteredProducts
+    };
+  }
+}
+</script>
+
+<style scoped>
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.card-title {
+  text-align: center;
+    margin-top: 5%;
+    margin-bottom: 2%;
+    font-size: 72px;
+    font-style: italic;
+    font-weight: bold;
+}
+
+.category-select {
+  text-align: center;
+  padding: 20px;
+}
+
+.category-layout {
+  font-size: 24px;
+  font-style: #000;
+  margin: 0 10px;
+  border: 1px solid #EDC2D8;
+  /* background-color: #EDC2D8; */
+  text-decoration: none; 
+  color: inherit;
+  padding: 5px 10px;
+}
+
+</style>
